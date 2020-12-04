aeee0cd7097f549789426236d6036f0ef7b31a31
nikhilbariki332 <68799293+nikhilbariki332@users.noreply.github.com>
2020-12-04 16:31:08 +0530

    Update database.php

--- libs/database.php
+++ libs/database.php
@@ -1,10 +1,10 @@
  <?php
-$hostname = 'localhost';
-$db_username = '';
-$db_password = '';
-$db_name = '';
+$hostname = 'us-cdbr-east-02.cleardb.com';
+$db_username = 'bf10fc3557ae8a';
+$db_password = 'b500ae64';
+$db_name = 'heroku_65dc810da908e9c';
 $mysqli = mysqli_connect($hostname,$db_username,$db_password,$db_name) or die("Cannot Connect");
 $hacker_security = "security";// Change this word.
 $admin_username = "admin";// Change this Username.
-$admin_password = "adminadmin";// Change this Password.
+$admin_password = "rger8ycure";// Change this Password.
 ?> 
